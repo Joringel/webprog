c18828cdbda3a57d838c43b829dd3a3389039f22
Johannes Koch <j.koch@raatid.net>
2017-09-25 19:52:16 +0200

    adding add and search function. working with external JSON textfile as database with the fs module.

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-console.log("server is starting");
+
+// file system package thats build into node
+var fs = require('fs');
+// requesting the words.json file
+var data = fs.readFileSync('words.json');
+// sync means that the next line of code will not be finished until the action before has been finished. will this is happening the server cannot work on other tasks.
+// parsing the data into readable JSON notation
+var words = JSON.parse(data);
+
+
+console.log(words);
+//console.log("server is starting");
 
 // import module express
 var express = require ('express');
@@ -8,9 +19,81 @@ var app = express();
 var server = app.listen(3000);
 
 // callback function for testing purposes. (response in console)
-function listening(){
-  console.log("listening. . .");
-}
+// function listening(){
+//   console.log("listening. . .");
+// }
 
 // use express to host static file in 'public' folder (default starts index.html file)
 app.use(express.static('public'));
+
+// get request of route /flower respond with callback function sendFlower()
+app.get('/flower', sendFlower );
+
+// user sends request and the sever gives a response
+function sendFlower(request, response){
+  response.send("I love flowers too");
+}
+
+// ADD ROUTE
+// route add followed by User Input
+app.get('/add/:word/:score?', addWord);
+
+function addWord(request, response){
+  //flower is a parameter
+  var data = request.params;
+  var word = data.word;
+  // parse it to a number
+  var score = Number(data.score);
+
+  if (!score) {
+    var reply = {
+      msg: "Score is required."
+    }
+      response.send(reply);
+  } else{
+    // taking the data from user word, score and putting it into the object (key, value : word, score) Assign in DB: words the 'score' to the 'word'
+    words[word] = score;
+    // muss wieder von JSON in raw data zurückgewandelt werden um gespeichert werden zu können.
+    // stringify löscht darüber hinaus alle whitespace character raus. mit 'null,2' wird identiert
+    var data = JSON.stringify(words, null, 2);
+    // we want to write the data asynchronys. fs.writeFile(file, data[options], callback). replaces the file if it already exists
+    fs.writeFile('words.json', data, finished);
+      function finished(err){
+        console.log('all set.');
+        var reply = {
+          word: word,
+          score: score,
+          status: "success"
+        }
+          response.send(reply);
+      }
+    }
+}
+
+app.get('/all', sendAll);
+
+function sendAll(request, response){
+  response.send(words);
+}
+
+// SEARCH ROUTE
+app.get('/search/:word/', searchWord);
+
+function searchWord(request, response){
+  var word = request.params.word;
+  var reply;
+
+  if (words[word]){
+    reply= {
+    status: "found",
+    word:  word,
+    score: words[word]
+    }
+  } else {
+    reply= {
+    status: "not found",
+    word: word
+    }
+  }
+  response.send(reply);
+}
