627a3dee3f592c2a93504f27fe19c7ce41669d08
Johannes Koch <j.koch@raatid.net>
2017-09-28 14:57:13 +0200

    no message

--- app/models/user.js
+++ app/models/user.js
@@ -8,11 +8,23 @@ var bcrypt   = require('bcrypt-nodejs');
 
 // SCHEMA for user Model =======================================================
 var userSchema = mongoose.Schema({
-  local: {
-    email     : String,
-    password  : String
-  }
-});
+        name: {
+          type: String,
+          required: true
+        },
+        password: {
+          type: String,
+          required: true
+        },
+        create_date:{
+          type: Date,
+          default: Date.nows
+        }
+  });
+
+// create model for users and expose to app
+var User = module.exports = mongoose.model('User', userSchema);
+
 
 // METHODS =====================================================================
 // generating hash
@@ -22,8 +34,58 @@ userSchema.methods.generateHash = function(password) {
 
 // check if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+    return bcrypt.compareSync(password, this.password);
 };
 
-// create model for users and expose to app
-module.exports = mongoose.model('User', userSchema);
+
+
+//
+// var mongoose = require('mongoose');
+//
+// var adminSchema = mongoose.Schema({ // required for the application, not for the actual database
+//       name: {
+//         type: String,
+//         required: true
+//       },
+//       password: {
+//         type: String,
+//         required: true
+//       },
+//       create_date:{
+//         type: Date,
+//         default: Date.nows
+//       }
+// });
+//
+// var Admin = module.exports = mongoose.model('Admin', adminSchema);
+//
+//
+// module.exports.getAdmins = function(callback, limit){ // get all Admins
+//       Admin.find(callback).limit(limit);
+// }
+//
+// module.exports.getAdminById = function(id, callback){ // get Admin
+//       Admin.findById(id, callback);
+// }
+//
+// module.exports.addAdmin = function(admin, callback){ // add Admin
+//       Admin.create(admin, callback);
+// }
+//
+// module.exports.updateAdmin = function(id, admin, options, callback){ // update Admin with id, admin-object, options and callback
+//       var query = {
+//             _id: id
+//       };
+//       var update = { // assign each field
+//             name: admin.name,
+//             password: admin.password
+//       }
+//       Admin.findOneAndUpdate(query, update, options, callback);
+// }
+//
+// module.exports.removeAdmin = function(id, callback){ // delete Admin
+//       var query = {
+//             _id: id
+//       };
+//       Admin.remove(query, callback);
+// }
